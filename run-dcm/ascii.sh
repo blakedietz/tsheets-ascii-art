57cf2840e407c5b8e550ff8c3cce3a4b08f8af0d
Blake Dietz <blakedietz@gmail.com>
2018-11-09 10:36:07 -0700

    feat(run dcm): Adds run dcm ascii art

---
+++ run-dcm/ascii.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Red text
+RT='\e[31m'
+# White text
+WT='\e[39m'
+# Black text
+BT='\e[30m'
+# Default color text
+DT='\e[39m' # No Color
+
+printf "${RT}██████████████████████████████${DT}\n"
+printf "${BT}█${WT}########${BT}██${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}████${WT}##${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}###${BT}███${WT}##${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}####${BT}██${WT}##${BT}█\n"
+printf "${BT}█${WT}########${BT}██${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}█${WT}##${BT}█${WT}##${BT}█\n"
+printf "${BT}█${WT}##${BT}███${WT}##${BT}███${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}██${WT}####${BT}█\n"
+printf "${BT}█${WT}##${BT}████${WT}##${BT}██${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}███${WT}###${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}██${WT}#######${BT}██${WT}##${BT}████${WT}##${BT}█\n"
+printf "${BT}█${WT}########${BT}███${WT}######${BT}██${WT}##${BT}█████${WT}##${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}████${WT}##${BT}█${WT}###${BT}███${WT}###${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}███████${WT}####${BT}█${WT}####${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}███████${WT}##${BT}█${WT}###${BT}█${WT}##${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}███████${WT}##${BT}█████${WT}##${BT}█\n"
+printf "${BT}█${WT}##${BT}█████${WT}##${BT}█${WT}##${BT}████${WT}##${BT}█${WT}##${BT}█████${WT}##${BT}█\n"
+printf "${BT}█${WT}########${BT}███${WT}######${BT}██${WT}##${BT}█████${WT}##${BT}█\n"
+printf "${RT}██████████████████████████████${DT}\n"
+
